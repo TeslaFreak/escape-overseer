4e3f8aa010fbf60c341a72a45ca6c60fe691de66
Chris Allmon <allmon@civicplus.com>
2018-09-28 00:06:32 -0500

    moved grommet css import to view manager

--- src/renderer/ControlView.js
+++ src/renderer/ControlView.js
@@ -6,8 +6,7 @@ import ClueCountControl from './ClueCountControl';
 import LiveViewControl from './LiveViewControl';
 import Tabs from 'grommet/components/Tabs';
 import Tab from 'grommet/components/Tab';
-import 'grommet/grommet.min.css';
-import App from 'grommet/components/App'
+import App from 'grommet/components/App';
 
 
 class ControlView extends Component {
