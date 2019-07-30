102dd8323238542832fdcc4e0b8b90ed3766cb0c
Chris Allmon <allmon@civicplus.com>
2019-07-29 23:45:35 -0500

    fixed icon spacing in clue counter

---
+++ .history/src/renderer/components/LiveViewEditor/EditPanels/CounterEditPanel_20190727142947.js
@@ -0,0 +1,252 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Slider from '@material-ui/lab/Slider';
+import PouchDB from 'pouchdb';
+import { withStyles, withTheme } from '@material-ui/core/styles';
+import classNames from 'classnames';
+import Dialog from '@material-ui/core/Dialog';
+import FontPicker from 'font-picker-react';
+import ChromePicker from 'react-color';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import AddIcon from '@material-ui/icons/Add';
+import TextFieldIcon from '@material-ui/icons/TextFields';
+import InsertPhotoIcon from '@material-ui/icons/InsertPhoto';
+import StarIcon from '@material-ui/icons/Star';
+import TimerIcon from '@material-ui/icons/Timer';
+import FormatAlignLeftIcon from '@material-ui/icons/FormatAlignLeft';
+import FormatAlignCenterIcon from '@material-ui/icons/FormatAlignCenter';
+import FormatAlignRightIcon from '@material-ui/icons/FormatAlignRight';
+import TextIcon from '@material-ui/icons/Title';
+import ColorIcon from '@material-ui/icons/ColorLens';
+import ClueIcon from '@material-ui/icons/Lock';
+import AddClueIcon from '@material-ui/icons/EnhancedEncryption';
+import Tooltip from '@material-ui/core/Tooltip';
+var WebFont = window.require('webfontloader');
+const electron = window.require('electron');
+const uuidv4 = require('uuid/v4');
+
+const appbarHeight = 64;
+
+const aspectRatio = 0.5625;
+const aspectWidthRatio = 1;
+const aspectHeightRatio = aspectRatio;
+const containerWidth = `calc(100vw - 280px - 80px - 140px - 70px)`;
+const containerHeight = `calc(100vh - ${appbarHeight}px - 200px)`;
+const aspectWidth = containerWidth * aspectWidthRatio;
+const aspectHeight = containerWidth * aspectHeightRatio;
+
+//google fonts API Key: AIzaSyDipkbeiVIwQoDKHnvmFCFQ1EoFW1_jw9E
+
+const styles = theme => ({
+    editPanelSubsectionHeader: {
+        color: '#dce0e3',
+        fontSize: '12px',
+        fontWeight: '600',
+        textTransform: 'uppercase',
+    },
+    controlElementLabel: {
+        fontSize: '12px',
+        fontWeight: '400',
+        color: '#dce0e3',
+        padding: '18px 0px 12px 0px',
+    },
+    ButtonRow: {
+        fontSize: '12px',
+        width: '100%',
+        padding: '12px 0px',
+    },
+    LeftRowButton: {
+        fontSize: '12px',
+        fontWeight: '600',
+        height: '40px',
+        borderRadius: '3px 0 0 3px',
+        backgroundColor: '#3e4c58',
+        color: '#fff',
+        opacity: '.5',
+        padding: '0px 10px',
+        '&:hover': {
+            opacity: 1,
+            backgroundColor: '#3e4c58',
+        },
+    },
+    CenterRowButton: {
+        fontSize: '12px',
+        fontWeight: '600',
+        height: '40px',
+        borderRadius: '0',
+        backgroundColor: '#3e4c58',
+        color: '#fff',
+        opacity: '.5',
+        padding: '0px 10px',
+        '&:hover': {
+            opacity: 1,
+            backgroundColor: '#3e4c58',
+        },
+    },
+    RightRowButton: {
+        fontSize: '12px',
+        fontWeight: '600',
+        height: '40px',
+        borderRadius: '0 3px 3px 0',
+        backgroundColor: '#3e4c58',
+        color: '#fff',
+        opacity: '.5',
+        padding: '0px 10px',
+        '&:hover': {
+            opacity: 1,
+            backgroundColor: '#3e4c58',
+        },
+    },
+    rowButtonSelected: {
+        backgroundColor: '#242c33',
+        opacity: 0.8,
+        '&:hover': {
+            backgroundColor: '#242c33',
+        }
+    },
+    fontPicker: {
+        width: '100%',
+        padding: '12px 0px',
+    },
+    fontSlider: {
+        paddingTop: '12px',
+    },
+    sliderContainer: {
+        padding: '12px 0px',
+    },
+    thumb: {
+        cursor: 'pointer',
+        background: '#35414c',
+        borderRadius: '100px',
+        boxShadow: '0 0 0 2px #f2f4f5  !important',
+    },
+    thumbWrapper: {
+        backgroundColor: '#fff',
+    },
+    track: {
+        background: '#6f7d86',
+        opacity: 1,
+    },
+});
+
+class ClueCountEditPanel extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state={iconSize: this.props.selectedItem.iconSize,
+                    iconSpacing: parseInt(this.props.selectedItem.iconSpacing**(1/1.7), 10)};
+        this.objects = [];
+        this.db = new PouchDB('kittens');
+        
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUpdate(nextProps) {
+        //TODO: separate out for each state variable. here and in each edit panel file.
+        if (this.state.fontSize !== nextProps.selectedItem.fontSize ) {
+            this.setState({iconSpacing: parseInt(this.props.selectedItem.iconSpacing**(1/1.7), 10)})
+        }
+    }
+
+    handleChange = (event, value, propertyName) => {
+        let roundedValue = value;
+        let displayValue = value;
+        switch(propertyName) {
+            case 'scale':
+                trueValue = parseInt(value, 10)**1.7;
+                displayValue = parseInt(value, 10);
+                break;
+            default:
+                roundedValue = value;
+                displayValue = value;
+        }
+        this.setState({[propertyName]: displayValue});
+        this.props.updateItemProperty(propertyName, roundedValue);
+    }
+
+    render() {
+        const { classes } = this.props;
+        return(
+            <Grid container direction='column' >
+                <Typography id="TimerHeader" className={classes.editPanelSubsectionHeader}>Clue Counter</Typography>
+                <Grid item >
+                    <Typography className={classes.controlElementLabel}>Number of Clues</Typography>
+                    <TextField type="number" defaultValue={3} onChange={(event) => this.handleChange(event, event.target.value, 'numberOfClues')}/>
+                </Grid>
+                <Typography className={classes.controlElementLabel}>Format</Typography>
+                <Grid item container direction='row' id='ButtonRow' className={classes.alignmentButtonRow}>
+                    <Grid item>
+                        <IconButton disableRipple className={classNames(classes.LeftRowButton, this.state.showHours ? classes.rowButtonSelected : '')} 
+                                    onClick={(event) => this.handleChange(event, !this.state.showHours, 'showHours')}> 
+                            Visual
+                        </IconButton>
+                    </Grid>
+                    <Grid item>
+                        <IconButton disableRipple className={classNames(classes.CenterRowButton, this.state.showMinutes ? classes.rowButtonSelected : '')} 
+                                    onClick={(event) => this.handleChange(event, !this.state.showMinutes, 'showMinutes')}>
+                            Numeric
+                        </IconButton>
+                    </Grid>
+                </Grid>
+                <Grid item >
+                    <Typography className={classes.controlElementLabel}>Set unused clue icon</Typography>
+                    <IconButton disableRipple className={classNames(classes.CenterRowButton, this.state.showMinutes ? classes.rowButtonSelected : '')} 
+                                    onClick={(event) => this.handleChange(event, !this.state.showMinutes, 'showMinutes')}>
+                            Change
+                    </IconButton>
+                </Grid>
+                <Grid item >
+                    <Typography className={classes.controlElementLabel}>Set used clue icon</Typography>
+                    <IconButton disableRipple className={classNames(classes.CenterRowButton, this.state.showMinutes ? classes.rowButtonSelected : '')} 
+                                    onClick={(event) => this.handleChange(event, !this.state.showMinutes, 'showMinutes')}>
+                            Change
+                    </IconButton>
+                </Grid>
+                <Grid item id='SizeSlider' >
+                    <Typography className={classes.controlElementLabel}>Icon Size</Typography>
+                    <Tooltip title={4} placement="top">
+                    <Slider
+                        classes={{
+                            thumb: classes.thumb,
+                            thumbWrapper: classes.thumbWrapper,
+                            track: classes.track,
+                          }}
+                        value={this.state.iconSize}
+                        min={1}
+                        max={100}
+                        onChange={(event, value) => this.handleChange(event, value, 'iconSize')}
+                    />
+                    </Tooltip>
+                </Grid>
+                <Grid item id='SpacingSlider' >
+                    <Typography className={classes.controlElementLabel}>Icon Spacing</Typography>
+                    <Tooltip title={4} placement="top">
+                    <Slider
+                        classes={{
+                            thumb: classes.thumb,
+                            thumbWrapper: classes.thumbWrapper,
+                            track: classes.track,
+                          }}
+                        value={this.state.iconSpacing}
+                        min={1}
+                        max={100}
+                        onChange={(event, value) => this.handleChange(event, value, 'iconSpacing')}
+                    />
+                    </Tooltip>
+                </Grid>
+            </Grid>
+        );
+    }
+}
+
+export default  withStyles(styles)(ClueCountEditPanel)
---
+++ .history/src/renderer/views/LiveView_20190201121351.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import Meter from 'grommet/components/Meter';
+import TimeDisplay from '../components/TimeDisplay';
+import ClueCountDisplay from '../components/ClueCountDisplay';
+import ClueDisplay from '../components/ClueDisplay';
+import classNames from 'classnames';
+import { withStyles, withTheme } from '@material-ui/core/styles';
+import Box from 'grommet/components/Box';
+import Image from '../../../assets/images/background.png';
+import PouchDB from 'pouchdb';
+import Paper from '@material-ui/core/Paper';
+import Dialog from '@material-ui/core/Dialog';
+import { Grid } from '@material-ui/core';
+const electron = window.require('electron')
+
+const styles = theme => ({
+  background: {
+      height: '100vh',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover'
+  },
+  alwaysVisible: {
+    paddingTop: '3%'
+  },
+  videoPlayer: {
+    objectFit: 'contain',
+    objectPosition: '50% 50%',
+    height: '100vh',
+    backgroundColor: 'black'
+  }
+});
+
+class LiveScreen extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { minutes: 60, seconds:0,
+                    clue1Used: false, clue2Used: false, clue3Used: false,
+                    clue: '',
+                    playVideo: false,
+                    selectedRoomId: null};
+
+    document.documentElement.style.overflow = 'hidden';
+    this.db = new PouchDB('kittens');
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.selectedRoomId != this.state.selectedRoomId) {
+        this.setBackground();
+    }
+  }
+
+  setBackground = () => {
+    this.db.getAttachment(this.state.selectedRoomId + '\\backgroundImg', 'backgroundImgFile').then(function(blob) {
+      var url = URL.createObjectURL(blob);
+      var backgroundDiv = document.getElementById('backgroundDiv');
+      backgroundDiv.style.backgroundImage= 'url('+ url +') ';
+    }).catch(function () {
+      //log error
+    })
+  }
+
+  playVideo = () => {
+    this.setState({playVideo: true});
+    return this.db.getAttachment(this.state.selectedRoomId + '\\breifVideo', 'breifVideoFile').then(function(blob) {
+        var url = URL.createObjectURL(blob);
+        var vidElement = document.getElementById('vid');
+        vidElement.src = url;
+    }).catch(function(err) {
+        this.closeVideo();
+    }.bind(this))
+  }
+
+  closeVideo = () => {
+    this.setState({playVideo:false});
+  }
+
+  handleClose = () => {
+    this.setState({ playVideo: false });
+  };
+
+  render() {
+    const { classes } = this.props;
+    electron.ipcRenderer.on('updateLiveViewTimeDisplay', (event, min, sec) => {
+      this.setState({minutes: min, seconds: sec});
+    });
+    electron.ipcRenderer.on('updateLiveViewClueDisplay', (event, clue) => {
+      this.setState({clue: clue});
+    });
+    electron.ipcRenderer.on('updateLiveViewClueCountDisplay', (event, clue1Used, clue2Used, clue3Used) => {
+      this.setState({clue1Used: clue1Used, clue2Used: clue2Used, clue3Used: clue3Used});
+    });
+    electron.ipcRenderer.on('roomSequence', (event, sequenceNodeId) => {
+      this.playVideo();
+    });
+    electron.ipcRenderer.on('updateSelectedRoomId', (event, selectedRoomId) => {
+      this.setState({selectedRoomId: selectedRoomId});
+      console.log(selectedRoomId);
+    });
+
+    return (
+      <div id='backgroundDiv' className={classNames(classes.background)}>
+          <Grid  container  direction='column' 
+              justify='flex-start'
+              alignItems='center'>
+              <Grid item className={classNames(classes.alwaysVisible)}>
+                <ClueCountDisplay selectedRoomId={this.state.selectedRoomId} liveScreen clue1Used={this.state.clue1Used} clue2Used={this.state.clue2Used} clue3Used={this.state.clue3Used} />
+                <Meter
+                  label = {<TimeDisplay selectedRoomId={this.state.selectedRoomId} liveScreen minutes={this.state.minutes} seconds={this.state.seconds} />}
+                  value = {this.state.minutes*60+this.state.seconds}
+                  type='circle'
+                  max={3600}
+                  size='large' />
+              </Grid>
+              <ClueDisplay liveScreen selectedRoomId={this.state.selectedRoomId} clue={this.state.clue} />
+            </Grid>
+            <Dialog
+                id='vidDialogue'
+                fullScreen
+                open={this.state.playVideo}
+                onClose={this.handleClose}
+            >
+                <video className={classNames(classes.videoPlayer)} id='vid' controls={false} autoPlay={true} onEnded={this.closeVideo} style={this.state.playVideo ? {} : { display: 'none' }}/>
+            </Dialog>
+            </div>
+    )
+  }
+
+  componentDidMount() {
+    
+  }
+
+  componentWillUnmount() {
+    
+  }
+}
+
+export default withStyles(styles)(LiveScreen);
